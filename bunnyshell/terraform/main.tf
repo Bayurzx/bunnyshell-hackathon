a9365fe22e9072c10f1bf4d7c51ecba27dcdf042
Bayurzx <bayurzx@gmail.com>
2023-07-10 10:54:09 +0100

    Fix: Updated main.tf to read aws keys

--- bunnyshell/terraform/main.tf
+++ bunnyshell/terraform/main.tf
@@ -11,7 +11,7 @@ terraform {
 
 
 provider "aws" {
-  profile = "terraform" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  # profile = "terraform" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
   region  = "us-east-1"
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
